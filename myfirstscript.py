167d9a0d8c5f652fd0657692d8cc3b09fc8dccfd
Nicholas Celotto <107053692+nickcelotto@users.noreply.github.com>
2024-09-04 15:12:30 -0500

    first commit

---
+++ myfirstscript.py
@@ -0,0 +1,16 @@
+# problem 1
+# What is missing in the while loop?
+# use a breakpoint in line 7 to debug
+
+x = 1
+while x < 10:
+    print(x)
+
+
+# problem 2
+# use a breakpoint in line 16 to debug
+
+mylist = list(range(5))
+
+for x in range(1,6):
+    print(f"Run No.:{mylist[x]}")
---
+++ python1.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+c = a+b
+
+for x in range(10):  # in a range of 10, values are 0-9
+    y = x * c
+
+
+message = "Hello World!"
+
+print(message)
